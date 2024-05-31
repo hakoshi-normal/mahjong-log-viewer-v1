2f6b7c7202cc5e3538de3fccc9cd63af8e372bfa
ノーマル箱四 <63761931+hakoshi-normal@users.noreply.github.com>
2024-05-31 16:06:19 +0900

    Update server.ts

--- server.ts
+++ server.ts
@@ -296,7 +296,7 @@ Deno.serve(async (req) => {
       addRowPos: "top", //when adding a new row, add it to the top of the table
       history: true, //allow undo and redo actions on the table
       pagination: "local", //paginate the data
-      paginationSize: 7, //allow 7 rows per page of data
+      paginationSize: 20, //allow 7 rows per page of data
       // paginationCounter:"rows", //display count of paginated rows in footer
       movableColumns: true, //allow column order to be changed
       initialSort: [ //set the initial sort order of the data
